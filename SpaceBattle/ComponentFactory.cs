842ac2ae58ef3eb1aab1bd967c373f65c54633ae
Luke Palmer <lrpalmer@gmail.com>
2009-02-26 02:54:23 -0700

    Remove testing hack.

--- SpaceBattle/ComponentFactory.cs
+++ SpaceBattle/ComponentFactory.cs
@@ -33,7 +33,7 @@ namespace SpaceBattle
     {
         class Entry
         {
-            public Entry(ComponentFactory<T> f, int a) { factory = f; ammo = 10; }
+            public Entry(ComponentFactory<T> f, int a) { factory = f; ammo = a; }
             public ComponentFactory<T> factory;
             public int ammo;
         };
