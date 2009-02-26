df47e385b317469e7c398ce665a35c0eb68b3d67
Luke Palmer <lrpalmer@gmail.com>
2009-02-26 04:47:01 -0700

    Added a bunch of sound files and indexing in the code.

--- SpaceBattle/Sequencer.cs
+++ SpaceBattle/Sequencer.cs
@@ -88,7 +88,21 @@ namespace SpaceBattle
                 engine.Update();
             }
         }
+    }
 
-        
+    static class Sounds
+    {
+        public static string[] Bongos = { "bongo1", "bongo2", "bongo3", "bongo4", "bongo5", "bongo6", "bongo7", "bongo8", "bongo9" };
+        public static string[] Clave = { "clave1", "clave2" };
+        public static string[] Conga = { "conga1", "conga2", "conga3", "conga4", "conga5", "conga6", "conga7", "conga8", "conga9", "conga10" };
+        public static string[] Cowbell = { "cowbell1", "cowbell2", "cowbell3", "cowbell4", "cowbell5", "cowbell6", "cowbell7", "cowbell8", "cowbell9", "cowbell10" };
+        public static string[] LightCym = { "cym1", "cym7", "cym9" };
+        public static string[] HiHat = { "hihat1", "hihat2", "hihat3", "hihat4", "hihat5", "hihat6", "hihat7", "hihat8", "hihat9", "hihat10" };
+        public static string[] Kettle = { "kett1", "kett2", "kett3", "kett4", "kett5" };
+        public static string[] ShortRattle = { "rattle1", "rattle5", "rattle7" };
+        public static string[] Tambourine = { "tambo1", "tambo2", "tambo3", "tambo4", "tambo5", "tambo6", "tambo7", "tambo8", "tambo9", "tambo10" };
+        public static string[] Triangle = { "tri2", "tri3", "tri4", "tri5" };
+        public static string[] Woodblock = { "woodblock1", "woodblock2", "woodpop1", "woodpop2" };
+
     }
 }
