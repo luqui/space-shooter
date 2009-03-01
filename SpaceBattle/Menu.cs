22ec13ffffad71ceaa7389c7636d40bcd1c549d3
Jude Allred <blinkymach12@gmail.com>
2009-03-01 06:26:25 -0700

    Added indicators for currently selected options.

--- SpaceBattle/Menu.cs
+++ SpaceBattle/Menu.cs
@@ -21,6 +21,7 @@ namespace SpaceBattle
             public string sText;
             public List<MenuItem> listSubItems;
             public Action fxnAction;
+            public Func<bool> fxnIsActive;
         }
         Stack<MenuItem> stack;
         MenuItem menu;
@@ -37,11 +38,17 @@ namespace SpaceBattle
             
             //Menu building helpers:
             Func<string, List<MenuItem>, Action, MenuItem> fxn = (s, list, action) => { return new MenuItem() { sText = s, listSubItems = list, fxnAction = action }; };
+            Func<string, List<MenuItem>, Action, Func<bool>, MenuItem> fxnWithPredicate = (s, list, action, isactive) => { var item = fxn(s, list, action); item.fxnIsActive = isactive; return item; };
             Action fxnEnter = () => 
             {
                 stack.Push(menu);
                 menu = menu.listSubItems[ixSubItems];
                 ixSubItems = 0;
+            };
+
+            Func<int,MenuItem> fxnParticleEffects = (count) => 
+            {
+                return fxnWithPredicate(count.ToString(),null,() => Explosion.CAP = count, () => {return Explosion.CAP == count;});
             };
 
             //Menu content:
@@ -53,11 +60,11 @@ namespace SpaceBattle
                 {
                    fxn("Maximum Particle Effects",new List<MenuItem>()
                    {
-                       fxn("250",null,() => Explosion.CAP = 250),
-                       fxn("500",null,() => Explosion.CAP = 500),
-                       fxn("1000",null,() => Explosion.CAP = 1000),
-                       fxn("1500",null,() => Explosion.CAP = 1500),
-                       fxn("Unlimited",null,() => Explosion.CAP = int.MaxValue),
+                       fxnParticleEffects(250),
+                       fxnParticleEffects(500),
+                       fxnParticleEffects(1000),
+                       fxnParticleEffects(1500),
+                       fxnWithPredicate("Unlimited",null,() => Explosion.CAP = int.MaxValue, () => {return Explosion.CAP == int.MaxValue;})
                    },fxnEnter)
                 }, fxnEnter),
                 fxn("Exit",null,() => Util.MODE  = Util.Mode.Exit)
@@ -117,13 +124,20 @@ namespace SpaceBattle
         {
             const float HEIGHT = 50.0f;
             const float TOP = 150.0f;
+            Color COLOR_SELECTED = Color.DarkRed;
+            Color COLOR_NORMAL = Color.White;
+            Color COLOR_ACTIVE = Color.Gold;
             //title:
             sb.DrawString(font,menu.sText, new Vector2(rectViewport.Width/4,50),Color.Gray, 0,Vector2.Zero,2*Vector2.One,SpriteEffects.None,0);
 
             //contents:
             for (var ixItems = 0; ixItems < menu.listSubItems.Count; ixItems++)
             {
-                var color = ixItems == ixSubItems ? Color.DarkRed : Color.White;
+                var color = ixItems == ixSubItems ? COLOR_SELECTED : COLOR_NORMAL;
+                if (color == COLOR_NORMAL && menu.listSubItems[ixItems].fxnIsActive != null)
+                    if (menu.listSubItems[ixItems].fxnIsActive())
+                        color = COLOR_ACTIVE;
+                
                 sb.DrawString(font, 
                     menu.listSubItems[ixItems].sText,
                     new Vector2(rectViewport.Width/4, TOP + ixItems * HEIGHT),
@@ -136,7 +150,7 @@ namespace SpaceBattle
                 var ixParentHighlight = stack.Peek().listSubItems.IndexOf(menu);
                 for (var ixItems = 0; ixItems < stack.Peek().listSubItems.Count; ixItems++)
                 {
-                    var color = ixItems == ixParentHighlight ? Color.DarkRed : Color.White;
+                    var color = ixItems == ixParentHighlight ? COLOR_SELECTED : COLOR_NORMAL;
                     sb.DrawString(font,
                         stack.Peek().listSubItems[ixItems].sText,
                         new Vector2(20, TOP + ixItems * HEIGHT/2),
@@ -153,10 +167,16 @@ namespace SpaceBattle
             {
                 for (var ixItems = 0; ixItems < menu.listSubItems[ixSubItems].listSubItems.Count; ixItems++)
                 {
+                    var color = COLOR_NORMAL;
+                    var fxn = menu.listSubItems[ixSubItems].listSubItems[ixItems].fxnIsActive;
+                    if (fxn != null)
+                        if (fxn())
+                            color = COLOR_ACTIVE;
+
                     sb.DrawString(font,
                         menu.listSubItems[ixSubItems].listSubItems[ixItems].sText,
                         new Vector2(3 * rectViewport.Width / 5, TOP + ixItems * HEIGHT / 2),
-                        Color.White,
+                        color,
                         0.0f,
                         Vector2.Zero,
                         0.5f,
